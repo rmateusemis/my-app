9235f2029ca64dd9dbf6d25e7024b7858bbea528
Roger Mateu <59694834+rmateusemis@users.noreply.github.com>
2020-06-03 17:16:10 +0200

    Tomando turnos

--- src/index.js
+++ src/index.js
@@ -1,30 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom'
 import './index.css';
 
+/*
 class Square extends React.Component {
+    
     constructor(props) { // Agregamos constructor para inicializar estado
         super(props);
         this.state = {
             value: null,
         };
     }
-
-    render() {
-        return (
-            <button className="square" 
-            onClick={() => // Modificamos estado onclick 're-renderiza'
-            this.setState({ value: 'X' })}
-            >
-            {this.state.value}
-            </button>
-        );
-    }
+    */
+function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
+
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({ squares: squares });
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
