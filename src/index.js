290eaa25e8eebc56dccd7b348d1dff13910a321d
Roger Mateu <59694834+rmateusemis@users.noreply.github.com>
2020-06-03 17:42:24 +0200

    Declarando un ganador

--- src/index.js
+++ src/index.js
@@ -2,16 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import './index.css';
 
-/*
-class Square extends React.Component {
-    
-    constructor(props) { // Agregamos constructor para inicializar estado
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-    */
 function Square(props) {
     return (
         <button
@@ -22,19 +12,22 @@ function Square(props) {
     );
 }
 
-
 class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squares: squares });
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -47,7 +40,8 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status =
+        `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
         return (
             <div>
